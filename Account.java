bfde419fb2e109415da7e9457f57eb0f45daf72e
Shin-kun <loewe_alivio@yahoo.com>
2016-09-27 09:07:47 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,3 +1,6 @@
+/**
+    File created by Michael Loewe Alivio
+**/
 public class Account{
 	private int accountNumber;
 	private double balance;
@@ -7,13 +10,16 @@ public class Account{
 		balance = 0.0;
 	}
 
-	Account(int anum, double bal){
-		accountNumber = anum;
+	Account(int aNum, double bal){
+	    if(aNum < 0){
+            throw new IllegalArgumentException("Your Account Number is a negative... Please change it..");
+	    }
+		accountNumber = aNum;
 		setBalance(bal);
 	}
 
-	Account(int anum){
-		accountNumber = anum;
+	Account(int aNum){
+		accountNumber = aNum;  //Jace: can this anum be rewritten as aNum, to follow camelcase convention?
 		balance = 0.0;
 	}
 
@@ -27,16 +33,14 @@ public class Account{
 
 	public void setBalance(double bal){
 		if(bal < 0.0){
-			throw new IllegalArgumentException("WONG!");
-
+			throw new IllegalArgumentException("WRONG set balance..negative value!");	//Jace: descriptive message, if possible
 		}
-
 		balance = bal;
 	}
 
 	public void credit(double amount){
 		if(amount < 0.0){
-			throw new IllegalArgumentException("WONG AGAIN!");
+			throw new IllegalArgumentException("WRONG set amount..negative value!"); //Jace: also here
 		}
 
 		balance = balance + amount;
@@ -44,7 +48,7 @@ public class Account{
 
 	public void debit(double amount){
 		if(amount < 0.0){
-			throw new IllegalArgumentException("WONG AGAIN and sayup!");
+			throw new IllegalArgumentException("WRONG set amount..negative value!"); //Jace: also here
 		}
 
 		if(balance >= amount){
@@ -53,9 +57,7 @@ public class Account{
 		}
 		else{
 			System.out.println("Amount Withdrawn exceeds the current balance!");
-
 		}
-
 	}
 
 	public String toString(){
