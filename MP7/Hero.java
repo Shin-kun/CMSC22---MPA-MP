654b111631e163df40e5a0f628c0e89d948e2924
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-01 12:33:24 +0800

    Create Hero.java

---
+++ MP7/Hero.java
@@ -0,0 +1,20 @@
+public abstract class Hero extends RPGCharacter{
+    //need changes and improvements
+    //gi erase nako ang levels coz i find no meaning sa pag butang og level tungod kay duel ra mn ni nga type of game
+    // private int level;
+
+        public Hero(String name) {
+            super(name);
+            //this.level = level;
+        }
+
+
+        // getters and setters
+        /*public int getLevel() {
+            return level;
+        }*/
+
+        /*public void setLevel(int level) {
+            this.level = level;
+        }*/
+}
