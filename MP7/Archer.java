9efbb5448c42f9c48846480afb28c0e4eff2340a
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-01 23:42:49 +0800

    Update Archer.java

--- MP7/Archer.java
+++ MP7/Archer.java
@@ -7,36 +7,45 @@ public class Archer extends Hero {
     //same stats as assassin
     //higher accuracy than assassin but weaker armor
     //need changes and improvements
-    private int BASE_ATTACK = 17;
-    private int ARMOR = 4;
+    private int BASE_ATTACK;
+    private int ARMOR;
 
     public Archer(String name){
         super(name);
         super.setHp(120);
         super.setMana(115);
+        BASE_ATTACK = 19;
+        ARMOR = 4;
     }
 
-    public int attack(){
-        return BASE_ATTACK;
+    public void skillDisp() {
+        System.out.println("Skill set (Hunter):\n" +
+                "1.Attack\n" +
+                "2.Poison Arrows\n" +
+                "3.Paralysis A\n");
+        System.out.println("Choose a skill: ");
     }
 
-    public int specAtt(int choice){
+
+    public int attack(int choice){
         if(choice == 1){
+            return BASE_ATTACK;
+        }
+        else if(choice == 2){
             //in every turn ma damage ang kontra let's say 5 turns
             //poison arrow
             //i add pa nig Damage per Second sa enemy
             super.minusMana(20);
             return BASE_ATTACK + 5;
         }
-        else if(choice == 2) {
+        else if(choice == 3) {
             //paralysis arrow
             //once in every turn ma paralyse ang enemy... cannot move
             super.minusMana(25);
             return BASE_ATTACK + 2;
         }
-        return choice;
+        return BASE_ATTACK;
     }
-
 
 
     public int takeDamage(int damage) {
