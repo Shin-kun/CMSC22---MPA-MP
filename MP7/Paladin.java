6daf59bb08b83685ca02af02423e48812c5c7b98
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-01 23:20:22 +0800

    Update Paladin.java

--- MP7/Paladin.java
+++ MP7/Paladin.java
@@ -7,22 +7,31 @@ public class Paladin extends Hero{
     //need changes and improvements
 
     // because of sword
-    private int BASE_ATTACK = 5;
+    private int BASE_ATTACK;
     // because of armor
-    private int ARMOR = 23;
+    private int ARMOR;
 
     public Paladin(String name) {
         // a swordsman will have +ARMOR for HP because of ARMOR
         super(name);
         super.setHp(150);
         super.setMana(110);
+        BASE_ATTACK = 5;
+        ARMOR = 23;
+    }
+    public void skillDisp() {
+        System.out.println("Skill set (Paladin):\n" +
+                "1. Heal");
+        System.out.println("Choose a skill: ");
     }
 
 
-
     //special attacks more or less mao ni ilang special moves
-    public int specAtt(int choice) {
+    public int attack(int choice) {
         if(choice == 1) {
+            return BASE_ATTACK;
+        }
+        else if(choice == 2) {
             int health = super.getHp();
             //freaken healll bloddy heelll
             if(health + 25 > 150){
@@ -33,16 +42,11 @@ public class Paladin extends Hero{
                 super.setHp(25 + health);
             }
             super.minusMana(17);
+            return 0;
         }
-        else if(choice == 2){
+        else if(choice == 3){
             //need more improvements
-
         }
-        return BASE_ATTACK;
-    }
-
-
-    public int attack() {
         return BASE_ATTACK;
     }
 
