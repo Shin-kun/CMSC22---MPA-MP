bd8d2f72fcd43364181de26f1ddf00612a0fb411
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-01 15:44:46 +0800

    Update Mage.java

--- MP7/Mage.java
+++ MP7/Mage.java
@@ -1,14 +1,31 @@
 public class Mage extends Hero {
-    private int BASE_ATTACK = 15;
+    //need changes and improvements
+    private int BASE_ATTACK = 8;
     private int ARMOR = 5;
 
     public Mage(String name){
         super(name);
-        super.setHp(17);
-        super.setMana(30);
+        super.setHp(117);
+        super.setMana(130);
     }
 
     public int attack() {
+        return BASE_ATTACK;
+    }
+
+    //special attacks more or less mao ni ilang special moves
+    public int specAtt(int choice){
+        if(choice == 1) {
+            //after 1 turn
+            //fireball
+            //highest amount of damage but greatest mana cost
+            super.minusMana(30);
+            return BASE_ATTACK + 20;
+        }
+        else if(choice == 2) {
+            //curse?? need improvements please
+            return BASE_ATTACK;
+        }
         return BASE_ATTACK;
     }
 
