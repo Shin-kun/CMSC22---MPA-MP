1f5ccd9a68001f67d5df740708578201499f2c28
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-01 23:23:09 +0800

    Update Hero.java

--- MP7/Hero.java
+++ MP7/Hero.java
@@ -1,13 +1,23 @@
+package LAB7;
+
+/**
+ * Created by niervin on 9/30/2016.
+ */
 public abstract class Hero extends RPGCharacter{
     //need changes and improvements
     //gi erase nako ang levels coz i find no meaning sa pag butang og level tungod kay duel ra mn ni nga type of game
     // private int level;
+        public static int buffcount1; //for counting buff turns
+        public static int buffcount2; //for counting buff turns
 
         public Hero(String name) {
             super(name);
+            buffcount1 = 0;
+            buffcount2 = 0;
             //this.level = level;
         }
 
+        public abstract void skillDisp();
 
         // getters and setters
         /*public int getLevel() {
@@ -17,4 +27,6 @@ public abstract class Hero extends RPGCharacter{
         /*public void setLevel(int level) {
             this.level = level;
         }*/
+        //public int getBuffcount1() { return buffcount1;  }
+        //public int getBuffcount2() { return buffcount2; }
 }
