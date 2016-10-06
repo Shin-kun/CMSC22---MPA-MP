46adf7c87f332ee75cf4c063f0ea4427b1f29135
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-06 19:12:39 +0800

    Update Assassin.java

--- MP7/Assassin.java
+++ MP7/Assassin.java
@@ -1,55 +1,101 @@
-package LAB7;
-
-/**
- * Created by niervin on 10/1/2016.
+/* latest edit ~6:35 PM 10/6/16
+ * Source code from Prof Nico Enego
+ * Created by Loewe Alivio, Michael Pacana and Jace Roldan
+ *  setting mana to 80
+ *  mikee change the stars please
+ *  slice and dice costs 35 per mana
+ *  plus mana per turn is now 8
  */
+
 public class Assassin extends Hero {
-    //need changes and improvements
-    private int BASE_ATTACK;
-    private int ARMOR;
+    public static final int BASE_ATTACK = 15;
+    public static final int BASE_ARMOR = 10;
 
     public Assassin(String name){
         super(name);
         super.setHp(100);
-        super.setMana(115);
-        BASE_ATTACK = 17;
-        ARMOR = 7;
+        super.setMana(80);
+
+        stats.attack= BASE_ATTACK;
+        stats.armor= BASE_ARMOR;
     }
 
     public void skillDisp() {
         System.out.println("Skill set (Assassin):\n" +
                 "1.Attack\n" +
-                "2.Poison Knives\n" +
-                "3.Smoke Bomb\n");  //subject to change
-        System.out.println("Choose a skill: ");
+                "2.Deadly Poison\n" +
+                "3.Slice and Dice\nChoose a skill:");  //subject to change
+    }
+
+    private void plusMana() {
+        if(getMana() + 8 < 80) {   //plus mana
+            super.plusMana(8);
+        }
+        else {
+            super.setMana(80);
+        }
+    }
+
+    private void buffturns(RPGCharacter opponent) {
+        if(buffcount1 != 0) {
+            buffcount1++;       //buffcount turns
+            opponent.damagePerSecond(5);
+            if(buffcount1 == 5) {
+                buffcount1 = 0;
+                opponent.restoreNormal(1);
+            }
+        }
     }
 
     //special attacks more or less mao ni ilang special moves
-    public int attack(int choice){
-        if(choice == 1) {
-            return BASE_ATTACK;
-        }
+    public void attack(int choice, RPGCharacter opponent){
+        plusMana();
+        buffturns(opponent);
+
+        opponent.restoreNormal(2); //restores paralysisStatus to false
+
         if(choice == 2) {
+            //highest DPS count
             //poison knives
             //similar to archer's poison arrows
             //dps
-            super.minusMana(20);
-            return BASE_ATTACK + 5;
-        }
-        else if(choice == 3) {
-            //di ko kahibaw unsaon pag butang ani
-            //accuracy.. smoke bomb
-            //100% accuracy and 40% accuracy for the enemy
+            // 5 turns
+            if(getMana() - 25 < 0 ) {
+                System.out.println("Mana is too low. Proceed to attack.");
+                opponent.takeDamage(stats.attack);
+            }
+
+            buffcount1 = 1;
+            opponent.takeEffect(1);
             super.minusMana(25);
-            return BASE_ATTACK;
+
+            opponent.takeDamage(stats.attack + 7);
         }
-        return BASE_ATTACK;
+
+        else if(choice == 3) {
+            //highest physical damage among hero types
+            if(getMana() - 35 < 0 ) {
+                System.out.println("Mana is too low. Proceed to attack.");
+                opponent.takeDamage(stats.attack);
+            }
+
+            super.minusMana(35);
+            opponent.takeDamage(stats.attack + 9);
+        }
+        else { opponent.takeDamage(stats.attack); }
     }
 
-    public int takeDamage(int damage) {
-        // reduce damage because of armor!!! oh yeah!
-        damage -= ARMOR;
-        // set new hp
-        return super.takeDamage(damage);
+    public void dispStats(){
+        System.out.println("The Elite Assassins of the Black Heart Syndicate are among the dangerous of their kind.\n" +
+                " These highly trained assassins perform their duties in a swift and silent manner leaving them no trace to follow.\n" +
+                " In the underground world, they are often hired to assassinate shady individuals.\n" +
+                " More often than not, they are successful in their operations.\n" +
+                " Usually, the corpses of their targets are testified to be in a zombified state.\n\n");
+
+        System.out.printf("Class: Assassin\n\tHP: ******\n\tMana: *****\n\tAttack: ***\n\tArmor: **");//15
+        System.out.println("\nSkills: \n\tDeadly Poison DPS damage + 5 for 4 turns\n\tSlice and Dice: Physical Damage + 7\n");
+    }
+    public String toString() {
+        return super.toString() + "\n\tBase attack = " + stats.attack + " Armor = " + stats.armor;
     }
 }
