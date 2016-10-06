e4ba499346185742cf6e2b895fe36c5fa20fb2b9
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-06 19:11:08 +0800

    Update Druid.java

--- MP7/Druid.java
+++ MP7/Druid.java
@@ -1,18 +1,20 @@
-package LAB7;
-
-/**
- * Created by niervin on 10/1/2016.
+/*latest edit ~6:43 PM 10/6/16
+ * Source code fron From Nico Enego
+ * Created by Loewe Alivio, Michael Pacana and Jace Roldan
+ *  Shocking Touch now costs 35 per use
+  * please check
  */
-public class Druid extends Hero{
-    private int BASE_ATTACK;
-    private int ARMOR;
+public class Druid extends Hero {
+    public static final int BASE_ATTACK = 10;
+    public static final int BASE_ARMOR = 5;
 
-    public Druid(String name){
+    public Druid(String name) {
         super(name);
         super.setHp(120);
-        super.setMana(95);
-        BASE_ATTACK = 10;
-        ARMOR = 5;
+        super.setMana(100);
+
+        stats.attack= BASE_ATTACK;
+        stats.armor= BASE_ARMOR;
     }
 
     public void skillDisp() {
@@ -24,72 +26,76 @@ public class Druid extends Hero{
     }
 
     private void plusMana() {   //plus mana in every turn
-        if(getMana() + 9 < 95) {
-            super.plusMana(9);
+        if(getMana() + 10 < 100) {
+            super.plusMana(10);
         }
         else {
-            setMana(95);
+            super.setMana(100);
         }
     }
+
     private void buffturns() {
         if(buffcount1 != 0){
-            buffcount1++;       //buffcount turns
-        }
-        if(buffcount1 == 5) {
-            ARMOR -= 6;
-            buffcount1 = 0;
-        }
-    }
-
-    private int changedAttack() {
-        if(buffcount1 == 0) {
-            return BASE_ATTACK;
-        }
-        else {
-            return BASE_ATTACK + 10;
+            buffcount1++;       //gs ctr increases
+            if(buffcount1 == 5){    //gserased
+                buffcount1 = 0;
+                stats.armor -= 6;
+                stats.attack -= 10;
+            }
         }
     }
 
-    public int attack(int choice) {
-        System.out.println(buffcount1 + " mao ni si buffcount1 ni hero!");
+    public void attack(int choice, RPGCharacter opponent) {
         plusMana();
         buffturns();
-        if(choice == 1){
-            return changedAttack();
-        }
-        else if(choice == 2) {
+
+        opponent.restoreNormal(2);
+        if(choice == 2) {
             //transformation
             //werewolf for 4 turns;
+
             if(getMana() - 15 < 0) {
                 System.out.println("Mana is too low. Proceed to attack.");
-                return changedAttack();
+                opponent.takeDamage(stats.attack);
             }
-            super.minusMana(15);
-            if(buffcount1 != 0) {
-                ARMOR -= 4;
+
+            if(buffcount1 == 0) {
+                super.minusMana(15);
+                stats.armor += 6;
+                stats.attack += 10;
             }
+
             buffcount1 = 1;
-            ARMOR += 6;
-            return 0;
+            isBuffed = true;
         }
         else if(choice == 3) {
             //shocking spell nature power
             //once every turn the enemy will paralyze.. the enemy won't move sa kana nga turn
             if(getMana() - 20 < 0) {
                 System.out.println("Mana is too low. Proceed to attack.");
-                return changedAttack();
+                opponent.takeDamage(stats.attack);
             }
-            super.minusMana(25);
-            chance = true; //it means the enemy is paralyzed
-            return BASE_ATTACK + 7;
+
+            super.minusMana(35);
+            opponent.takeEffect(2); //it means the enemy is paralyzed
+            opponent.takeDamage(stats.attack + 7);
         }
-        return BASE_ATTACK;
+
+        else { opponent.takeDamage(stats.attack); }
     }
 
-    public int takeDamage(int damage) {
-        // reduce damage because of armor!!! oh yeah!
-        damage -= ARMOR;
-        // set new hp
-        return super.takeDamage(damage);
+    public void dispStats(){
+        System.out.println("The Ra-Ahaya Druids of The Great Alfatrar Forest are among " +
+                "the most vicious druids in the world of Exios.\n" +
+                " They are feared for their Werewolf Aspect bestowed " +
+                "upon by the Cursed Dark God, Vala.\n" +
+                " Their vicious and cannibalistic ways make them one of the greatest threats in Exios.\n\n");
+
+        System.out.printf("Class: Druid\n\tHP: ******\n\tMana: *****\n\tAttack: **\n\tArmor: *");//15
+        System.out.println("\nSkills: \n\tWerewolf Aspect: Additional Armor for 5 turns\n\tShocking Touch: Opponent is unable to move for 5 turns\n");
+    }
+
+    public String toString() {
+        return super.toString() + "\n\tBase attack = " + stats.attack + " Armor = " + stats.armor;
     }
 }
