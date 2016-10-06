1ed7e7f10262c40fd033413c9efbe08951718a47
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-06 19:15:16 +0800

    Update Mage.java

--- MP7/Mage.java
+++ MP7/Mage.java
@@ -1,50 +1,94 @@
-package LAB7;
-
-/**
- * Created by niervin on 10/1/2016.
+/* latest edit ~6:46 PM 10/6/16
+ * Source code from Prof Nico Enego
+ * Created by Loewe Alivio, Michael Pacana and Jace Roldan
+ *  mage fireball damage is now + 12
+ *  setting HP to 100
+ *  mike ang stars pleaseeee
  */
 public class Mage extends Hero {
-    //need changes and improvements
-    private int BASE_ATTACK;
-    private int ARMOR;
+    public static final int BASE_ATTACK = 5;
+    public static final int BASE_ARMOR = 5;
 
     public Mage(String name){
         super(name);
-        super.setHp(117);
-        super.setMana(130);
-        BASE_ATTACK = 8;
-        ARMOR = 8;
+        super.setHp(100);
+        super.setMana(140);
+
+        stats.attack= BASE_ATTACK;
+        stats.armor= BASE_ARMOR;
     }
 
     public void skillDisp() {
         System.out.println("Skill set (Mage):\n" +
-                "1.Fireball\n" +
-                "2.Curse");
-        System.out.println("Choose a skill:\n");
-    }
-    //special attacks more or less mao ni ilang special moves
-    public int attack(int choice) {
-        if(choice == 1){
-            return BASE_ATTACK;
-        }
-        else if(choice == 2) {
-            //after 1 turn
-            //fireball
-            //highest amount of damage but greatest mana cost
-            super.minusMana(30);
-            return BASE_ATTACK + 20;
-        }
-        else if(choice == 3) {
-            //curse?? need improvements please
-            return BASE_ATTACK;
-        }
-        return BASE_ATTACK;
+                "1.Attack\n" +
+                "2.Fireball\n" +
+                "3.Curse of Madness");
+        System.out.print("Choose a skill: ");
     }
 
-    public int takeDamage(int damage) {
-        // reduce damage because of armor!!! oh yeah!
-        damage -= ARMOR;
-        // set new hp
-        return super.takeDamage(damage);
+    private void plusMana() {   //plus mana in every turn
+        if(getMana() + 7 < 140) {
+            super.plusMana(7);
+        }
+        else {
+            super.setMana(140);
+        }
+    }
+
+    private void buffturns(RPGCharacter opponent) {
+        if(buffcount2 != 0) {
+            buffcount2++;       //buffcount turns
+            opponent.damagePerSecond(8);
+            if(buffcount2 == 4) {
+                buffcount2 = 0;
+                opponent.restoreNormal(1);
+            }
+        }
+
+    }
+
+
+    //special attacks more or less mao ni ilang special moves
+    public void attack(int choice,RPGCharacter opponent) {
+        plusMana();
+        buffturns(opponent);
+
+        opponent.restoreNormal(2);
+
+        if(choice == 2) {
+            if(getMana() - 35 < 0 ) {
+                System.out.println("Mana is too low. Proceed to attack.");
+                opponent.takeDamage(stats.attack);
+            }
+
+            super.minusMana(35);
+            opponent.takeDamage(stats.attack + 12);
+        }
+        else if(choice == 3) {
+            if(getMana() - 20 < 0) {
+                System.out.println("Mana is too low. Proceed to attack.");
+                opponent.takeDamage(stats.attack);
+            }
+
+            super.minusMana(20);
+            buffcount2 = 1;
+            opponent.takeEffect(1);
+
+            opponent.takeDamage(stats.attack + 5);
+        }
+
+        else { opponent.takeDamage(stats.attack); }
+    }
+
+    public void dispStats(){
+        System.out.println("The Mages of the Daran studied the ways of the Arcane.\n" +
+                " Their mastery over magic is constantly sought by the countries of Exios for it is a strong asset in the battlefield.\n" +
+                " But, these mages are often secluded in the outside world in order for them to continue pursue the secrets of the Arcane.\n\n");
+
+        System.out.printf("Class: Mage\n\tHP: ******\n\tMana: *******\n\tAttack: *\n\tArmor: *");//15
+        System.out.println("\nSkills: \n\tFireball: Flame Damage Attack \n\tCurse of Madness: Curse Damage for 4 turns\n");
+    }
+    public String toString() {
+        return super.toString() + "\n\tBase attack = " + stats.attack + " Armor = " + stats.armor;
     }
 }
