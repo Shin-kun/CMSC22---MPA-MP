2d0cc2b01b00ec1fa23f5987df1241c30d057019
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2016-10-06 19:18:15 +0800

    Update Paladin.java

--- MP7/Paladin.java
+++ MP7/Paladin.java
@@ -1,60 +1,101 @@
-package LAB7;
-
-/**
- * Created by niervin on 10/1/2016.
+/* latest edit ~6:50 PM 10/6/16
+ * Source code from Prof Nico Enego
+ * Created by Loewe Alivio, Michael Pacana and Jace Roldan
+ *  setting damage to 15..
+ *  Righteous Shield now costs 30 mana per use
+ *  plus mana is now 5
  */
-public class Paladin extends Hero{
-    //need changes and improvements
 
-    // because of sword
-    private int BASE_ATTACK;
-    // because of armor
-    private int ARMOR;
+public class Paladin extends Hero{
+    public static final int BASE_ATTACK = 15;
+    public static final int BASE_ARMOR = 15;
 
     public Paladin(String name) {
-        // a swordsman will have +ARMOR for HP because of ARMOR
         super(name);
-        super.setHp(150);
-        super.setMana(110);
-        BASE_ATTACK = 5;
-        ARMOR = 23;
+        super.setHp(140);
+        super.setMana(60);
+
+        stats.attack = BASE_ATTACK;    //call overloaded constructor for stats.java
+        stats.armor = BASE_ARMOR;
     }
+
     public void skillDisp() {
         System.out.println("Skill set (Paladin):\n" +
-                "1. Heal");
-        System.out.println("Choose a skill: ");
+                "1. Attack\n"+
+                "2. Divine Blessing\n" +
+                "3. Righteous Shield");
+        System.out.print("Choose a skill: ");
     }
 
-
-    //special attacks more or less mao ni ilang special moves
-    public int attack(int choice) {
-        if(choice == 1) {
-            return BASE_ATTACK;
+    private void plusMana() {   //regen mana every turn
+        if(getMana() + 5 < 60) {
+            super.plusMana(5);
         }
-        else if(choice == 2) {
+        else {
+            super.setMana(60);
+        }
+    }
+
+    private void buffturns() {
+        if(buffcount2 != 0) {
+            buffcount2++;
+            if(buffcount2 == 5) {
+                buffcount2 = 0;
+                stats.armor = BASE_ARMOR;
+            }
+        }
+    }
+
+    public void attack(int choice,RPGCharacter opponent) {//attacks & special attacks
+        plusMana();
+        buffturns();
+
+        opponent.restoreNormal(2);
+
+        if(choice == 2) {  //heal
+            if(getMana() - 17 < 0) {
+                System.out.println("Mana is too low. Proceed to attack.");
+                opponent.takeDamage(stats.attack);
+            }
+
             int health = super.getHp();
-            //freaken healll bloddy heelll
-            if(health + 25 > 150){
-                int points = 150 - health;
-                super.setHp(points + health);
+            if(health + 25 > 140) {
+                super.setHp(140);
             }
             else{
                 super.setHp(25 + health);
             }
+
             super.minusMana(17);
-            return 0;
         }
-        else if(choice == 3){
-            //need more improvements
+
+        else if(choice == 3){ //armor buff
+            if(getMana() - 20 < 0) {
+                System.out.println("Mana is too low. Proceed to attack.");
+                opponent.takeDamage(stats.attack);
+            }
+
+            else if(buffcount2 == 0) {
+                super.minusMana(30);
+                stats.armor += 10;
+            }
+
+            buffcount2 = 1;
+            isBuffed = true;
         }
-        return BASE_ATTACK;
+        else { opponent.takeDamage(stats.attack); }
     }
 
-    public int takeDamage(int damage) {
-        // reduce damage because of armor!!! oh yeah!
-        damage -= ARMOR;
-        // set new hp
-        return super.takeDamage(damage);
+    public void dispStats(){
+        System.out.println("The Kalyos Paladins pledged their oath and loyalty to the God Father, Sindros, as their testament to follow the path of righteousness.\n" +
+                " Oftentimes, they are sent by the Kalyos Empire to eradicate monsters that threaten to destroy the country as one of its elite forces.\n" +
+                " Their charismatic fervor in the battlefield gave their allies a boost to their morale, and enemies and monsters alike shudder at their inevitable death.\n\n");
+
+        System.out.printf("Class: Paladin\n\tHP: *******\n\tMana: ***\n\tAttack: **\n\tArmor: ****");//17
+        System.out.println("\nSkills: \n\tDivine Blessing: Heal\n\tRighteous shield: Buffs Armor for 5 turns\n");
     }
 
+    public String toString(){
+        return super.toString() + "\n\tBase attack = " + stats.attack + " Armor = " + stats.armor;
+    }
 }
